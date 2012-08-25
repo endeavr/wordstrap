bf9a89a9141444e483495463a0a6757b0f6cdfb1
Joel Kuczmarski <leoj3n@gmail.com>
2012-08-25 08:27:49 -0500

    Now using Theme Hook Alliance

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,10 @@
+<?php tha_footer_before(); ?>
 <footer id="content-info" class="container" role="contentinfo">
+  <?php tha_footer_top(); ?>
   <?php dynamic_sidebar('sidebar-footer'); ?>
   <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+  <?php tha_footer_bottom(); ?>
 </footer>
+<?php tha_footer_after(); ?>
 
 <?php wp_footer(); ?>
--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,10 @@
 <header id="banner" role="banner">
+  <?php tha_header_top(); ?>
   <div class="container">
     <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     <nav id="nav-main" role="navigation">
       <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills')); ?>
     </nav>
   </div>
+  <?php tha_header_bottom(); ?>
 </header>
