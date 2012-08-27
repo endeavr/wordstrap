65f9de087ee67227ca3fac1b248a257217d5598e
Joel Kuczmarski <leoj3n@gmail.com>
2012-08-27 02:22:55 -0500

    Merge files from "master" branch

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ define('WP_BASE',                   wp_base_dir());
 define('THEME_NAME',                next($get_theme_name));
 define('RELATIVE_PLUGIN_PATH',      str_replace(site_url() . '/', '', plugins_url()));
 define('FULL_RELATIVE_PLUGIN_PATH', WP_BASE . '/' . RELATIVE_PLUGIN_PATH);
-define('RELATIVE_CONTENT_PATH',     str_replace(site_url(), '', content_url()));
+define('RELATIVE_CONTENT_PATH',     str_replace(site_url() . '/', '', content_url()));
 define('THEME_PATH',                RELATIVE_CONTENT_PATH . '/themes/' . THEME_NAME);
 
 define('CHILD_THEME_NAME',          next(explode('/themes/', get_stylesheet_directory())));
