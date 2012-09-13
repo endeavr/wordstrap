302fa25630818e05814b5435fd35aad43b33d85a
Jason Loftis <jason@jloft.com>
2012-09-13 12:20:21 -0700

    lib/custom.php
    Added an action function that makes the Theme Customizer more visible by placing a link for it under the Appearances menu in the control panel.
    
    http://ottopress.com/2012/theme-customizer-part-deux-getting-rid-of-options-pages/
    Signed-off-by: Jason Loftis <jason@jloft.com>

--- lib/custom.php
+++ lib/custom.php
@@ -8,12 +8,18 @@ if (!defined('__DIR__')) { define('__DIR__', dirname(__FILE__)); }
    	include_once locate_template('/admin/functions.php');
    // Include the options.php file directly in order to enable compatibility with the WordPress Front End Theme Customizer.
    	require_once locate_template('/admin/options.php');
- 
+   // Make the Theme Customizer more visible
+   	add_action ('admin_menu', 'themedemo_admin');
+   		function themedemo_admin() {
+	   		// add the Customize link to the admin menu
+	   		add_theme_page( 'Customize', 'Customize', 'edit_theme_options', 'customize.php' );
+	   	}
+	 
    // WP-LESS
      add_action('wp_enqueue_scripts', 'ws_wp_less');
      if ( ! function_exists( 'ws_wp_less' ) ):
-     function ws_wp_less() {
-     /* Load Bootstrap CSS */
-       require_once locate_template('/lib/wp-less/bootstrap-for-theme.php'); 
-     }
+     	function ws_wp_less() {
+     		/* Load Bootstrap CSS */	     
+     		require_once locate_template('/lib/wp-less/bootstrap-for-theme.php'); 
+     	}
      endif;
