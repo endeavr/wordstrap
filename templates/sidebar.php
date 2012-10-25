8d75711db0e76317fc745737be338013729b234b
Jason Loftis <jason@jloft.com>
2012-10-24 21:41:33 -0700

    EDIT templates/sidebar.php
    Complete overhaul.
    
    Moved all of the sidebar properties from the base.php file into the sidebar.php file.
    
    A custom ws_sidebar_class() was added.
    
    Signed-off-by: Jason Loftis <jason@jloft.com>

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,7 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php tha_sidebars_before(); ?>
+<aside id="sidebar" <?php ws_sidebar_class(); ?> role="complementary">
+	<?php tha_sidebar_top(); ?>
+     	<?php dynamic_sidebar('sidebar-primary'); ?>
+     <?php tha_sidebar_bottom(); ?>
+</aside>
+<?php tha_sidebars_after(); ?>
