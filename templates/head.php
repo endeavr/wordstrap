c1c6e2e8c8cd30a2be11800b2ae65ea9876252bb
Jason Loftis <jason@jloft.com>
2012-10-24 21:38:36 -0700

    EDIT templates/head.php
    Minor reorganization of elements.
    
    Signed-off-by: Jason Loftis <jason@jloft.com>

--- templates/head.php
+++ templates/head.php
@@ -9,15 +9,16 @@
   <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-
-  <?php tha_head_bottom(); ?>
   <?php wp_head(); ?>
 
   <?php if (have_posts()) : ?>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name') ?> Feed" href="<?php echo home_url() ?>/feed/">
   <?php endif; ?>
+  
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+  
+  <?php tha_head_bottom(); ?>
 </head>
