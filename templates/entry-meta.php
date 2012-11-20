50b2064aae26c216e170880b8a3df863d4160095
Jason Loftis <jason@jloft.com>
2012-11-19 16:38:48 -0800

    MODIFY templates/entry-meta.php
    Surrounded the by line and date with a small well.
    
    Signed-off-by: Jason Loftis <jason@jloft.com>

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,10 @@
-<time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()); ?></time>
-<p class="byline author vcard"><?php echo __('Written by', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
+<div class="well well-small">
+	<time class="updated pull-left" datetime="<?php echo get_the_time('c'); ?>" pubdate>
+		<i class="icon-calendar"></i>&nbsp;
+		<?php echo sprintf(__('Posted on %s', 'roots'), get_the_date(), get_the_time()); ?>
+	</time>
+	<p class="byline author vcard pull-left">
+		<i class="icon-user"></i>&nbsp;	
+		<?php echo __('Written by', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
+	</p>
+</div>
